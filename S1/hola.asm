1ab0f84a4fc8d7557392cda2555f75e6c805ad12
Manuel Fernández La-Chica <manferlac@cvi041190.ugr.es>
2020-02-26 12:08:43 +0100

    Añadir Seminario 1

---
+++ S1/hola.asm
@@ -0,0 +1,27 @@
+pila segment stack 'stack'
+	dw 100h dup (?)
+pila ends
+datos segment 'data'
+	msg db 'hola $'
+datos ends
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
+		mov cx,0
+		bucle:
+			mov dx,OFFSET msg
+			mov ah,9
+			int 21h
+			;actualizar contador y comprobar condicion
+			inc cx
+			cmp cx,7
+			jne bucle
+
+		mov ax,4C00h
+		int 21h
+	main ENDP
+codigo ends
+
+END main
